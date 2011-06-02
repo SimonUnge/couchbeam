8ca9c2d1aa5079fd064272fee355aea8f0fd1996
Simon <simon.unge.1641@student.uu.se>
2011-06-02 14:50:21 +0200

    Now a supervisor exists, that restarts WM and Listener.

---
+++ src/listener_sup.erl
@@ -0,0 +1,50 @@
+-module (listener_sup).
+-include ("couchbeam.hrl").
+
+-export ([start/3, start/1]).
+
+start(Host, Port, Name) ->
+  application:start(sasl),
+  application:start(ibrowse),
+  application:start(crypto),
+  application:start(couchbeam),
+  Server = couchbeam:server_connection(Host, Port),
+  {ok, Db} = couchbeam:open_or_create_db(Server, Name),
+  WorkManagerPid = start_workmanager(Db), 
+  ListenerPid = start_listener(Db, WorkManagerPid),%I do not use workmanagerpid, do I?
+  receive_loop(WorkManagerPid, ListenerPid, Db).
+
+%%Just for testing purposes.
+start(Database) when is_atom(Database)->
+  case Database of
+    reg_a ->
+      start("localhost", 5002, "reg_a");
+    reg_b ->
+      start("localhost", 5003, "reg_b");
+    global_node ->
+      start("localhost", 5001, "global_node")
+  end.
+
+receive_loop(WorkManagerPid, ListenerPid, Db) ->
+  receive
+    {'EXIT', WorkManagerPid, Reason} ->
+      io:format("WM ~p died, reason ~p. Restarts.~n", [WorkManagerPid, Reason]),
+      NewWorkManagerPid = start_workmanager(Db),
+      ListenerPid ! {workmanagerpid, NewWorkManagerPid},
+      receive_loop(NewWorkManagerPid, ListenerPid, Db);
+    {'Exit', ListenerPid, Reason} ->
+      io:format("Listener ~p died, reason ~p. Restarts.~n", [ListenerPid, Reason]),
+      NewListenerPid = start_listener(Db, WorkManagerPid),
+      receive_loop(WorkManagerPid, NewListenerPid, Db)
+  end.
+
+start_workmanager(Db) ->
+  process_flag(trap_exit, true),
+  WorkManagerPid = spawn_link(workmanager, work_manager, [5, Db]),
+  WorkManagerPid.
+
+start_listener(Db, WorkManagerPid) ->
+  process_flag(trap_exit, true),
+  spawn_link(listener, start, [Db, WorkManagerPid]).
+  
+  
